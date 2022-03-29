2c5c2cc32fbbfad38eada7abd092b1ebd959f1be
atctk <80257556+atctk@users.noreply.github.com>
2022-03-29 10:50:26 +0000

    03/29

--- indeed.py
+++ indeed.py
@@ -2,25 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 
 LIMIT = 50
-URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}&start=9999"
+URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={LIMIT}"
+
 def extract_indeed_pages():
-  pages =[]
-  result =         requests.get(URL)
-  soup =   BeautifulSoup(result.text, 'html.parser')
+  result = requests.get(URL)
+  soup = BeautifulSoup(result.text, "html.parser")
+  pagination = soup.find("div",{"class":"pagination"})
 
-  pagination=soup.find("div",{"class":"pagination"})
-  links = pagination.find_all('a')
-  pages=[]
-  for link in links:
-    pages.append(link.string)
+  links = pagination.find_all("a")
 
-  max_page = int(pages[1])+1
+  pages = []
+  for link in links[:-1]:
+    pages.append(int(link.string))
+  max_page = pages[-1]
   return max_page
-
 
 def extract_indeed_jobs(last_page):
   jobs = []
-  for page in range(last_page):
-    result=requests.get(f"{URL}&start={page*LIMIT}")
-    print(result.status_code)
+  #for page in range(last_page):
+  result = requests.get(f"{URL}&start={0 * LIMIT}")
+  soup = BeautifulSoup(result.text, "html.parser")
+  results = soup.find_all("div", {"class": "heading4"})
+  for result in results:
+    title = result.find("h2", {"class": "jobTitle"}).find("span", title=True).string
+    print(title)
   return jobs
